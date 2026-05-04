79548df15f6009094389e4997e3e282a715d3d62
HarshithaDN123 <harshithadn95@gmail.com>
2026-05-04 15:36:47 +0530

    Initial Commit

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { clearAuth, getAuth } from "../utils/storage";
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
 const axiosClient = axios.create({
 baseURL,
